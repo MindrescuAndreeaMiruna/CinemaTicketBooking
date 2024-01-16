934213bc3074924cb3eb83a8014b4f4bed9c179a
MindrescuAndreeaMiruna <andreeamiruna2703@yahoo,com>
2024-01-16 17:53:57 +0200

    1st commit

---
+++ CinemaTicketBooking/CinemaTicketBooking/Models/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmTicketBooking.Models
+{
+    public class Client
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+
+        public string LastName { get; set; }
+
+        [Range(0, 50)]
+
+        public int Age { get; set; }
+
+        [StringLength(10)]
+
+        public string? TelephoneNumber { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+
+
+
+    }
+}
